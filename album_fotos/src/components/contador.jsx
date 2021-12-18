d02766206a94d71e4a03f73bb09e163f36d12ef5
User <neilsonalves4@gmail.com>
2021-12-18 20:14:03 -0300

    criando botão e deixando ele funcionavel com a função 'tratarIncremento', e formatando com bootstrap e uma formatação pesonalizada com condicional com a função 'formataContador'

--- album_fotos/src/components/contador.jsx
+++ album_fotos/src/components/contador.jsx
@@ -2,12 +2,27 @@ import React, { Component} from "react";
 
 class Contador extends Component {
     state = {contador: 0};//declarando um contador
+    estilos = {
+        fontSize: 40,
+        fontWeight: "normal"
+    }
 
     render() {
         return(
-            
+            <React.Fragment>
+                <span style={this.estilos} className="badge badge-primary m-2">{this.formataContador()}</span>
+                <button className="btn btn-secondary" onClick={this.tratarIncremento}>Incrementar</button>
+            </React.Fragment>
         );
     }
+    formataContador(){
+        const {contador} = this.state;
+        return contador === 0 ? "Zero": contador;
+    }
+    tratarIncremento = () => {
+        this.setState({contador: this.state.contador + 1});
+    }
+
 }
 
 export default Contador;
--- album_fotos/src/index.js
+++ album_fotos/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+//import './index.css';
+//import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.css'
+import Contador from './components/contador';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Contador />
   </React.StrictMode>,
   document.getElementById('root')
 );
