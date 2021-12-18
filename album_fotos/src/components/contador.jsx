0a2fbc4a78da7c9df265777b44379bf7ed8c5f82
User <neilsonalves4@gmail.com>
2021-12-18 19:17:57 -0300

    criando a base para o contador

---
+++ album_fotos/src/components/contador.jsx
@@ -0,0 +1,13 @@
+import React, { Component} from "react";
+
+class Contador extends Component {
+    state = {contador: 0};//declarando um contador
+
+    render() {
+        return(
+            
+        );
+    }
+}
+
+export default Contador;
